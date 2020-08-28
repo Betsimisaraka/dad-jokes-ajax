727dcb5c76ea2abefc27227cf44d934f04223860
Anita RASOA <anita.ras@onja.org>
2020-08-28 14:02:47 +0300

    Creating dad jokes

--- jokes.js
+++ jokes.js
@@ -1,3 +1,6 @@
+const jokeButton = document.querySelector('.getJoke');
+const jokeHolder = document.querySelector('.joke p');
+
 const buttonText = [
 	'Ugh.',
 	'🤦🏻‍♂️',
@@ -8,3 +11,29 @@ const buttonText = [
 	'please stop',
 	'that was the worst one',
 ];
+
+function randomItemFromArray(arr, not) {
+	const item = arr[Math.floor(Math.random() * arr.length)];
+	if (item === not) {
+		return randomItemFromArray(arr, not);
+	}
+	return item;
+}
+
+async function fetchJoke() {
+	const response = await fetch('https://icanhazdadjoke.com', {
+		headers: {
+			Accept: 'application/json',
+		}
+	});
+	const data = await response.json();
+	return data;
+}
+
+async function handleClick() {
+	const { joke } = await fetchJoke();
+	jokeHolder.textContent = joke;
+	jokeButton.textContent = randomItemFromArray(buttonText, jokeButton.textContent);
+}
+
+jokeButton.addEventListener('click', handleClick);
